da1ebd90b316c1029b4bb56c9b3ea5c5b77459f8
lemoncastle <yangadams123@gmail.com>
2025-01-22 14:37:12 -0800

    nav bar fix 1.5

--- global.js
+++ global.js
@@ -5,7 +5,7 @@ function $$(selector, context = document) {
 }
 
 let pages = [ // this is an array of elements where each element has url and title
-    { url: 'index.html', title: 'Home' },
+    { url: '', title: 'Home' },
     { url: 'projects/', title: 'Projects' },
     { url: 'resume/', title: 'Resume' },
     { url: 'contact/', title: 'Contact' },
